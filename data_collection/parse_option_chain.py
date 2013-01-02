84d69414d1b7f3fafe11a3513831edcb39740cee
gazzman <sgazzisg@uw>
2013-01-02 13:43:53 -0500

    Made a proper python command line script and added support for quarterlies.

--- data_collection/parse_option_chain.py
+++ data_collection/parse_option_chain.py
@@ -14,8 +14,6 @@ from sqlalchemy.dialects.postgresql import (BOOLEAN, CHAR, DATE, INTEGER,
                                             NUMERIC, VARCHAR)
 
 
-file_to_parse = sys.argv[1]
-db_name = sys.argv[2]
 
 Base = declarative_base()
 
@@ -94,6 +92,15 @@ def get_expiry_date(last_date, data_date, expiry):
     if 'week' in expiry.lower():
         if last_date: return last_date + timedelta(days=7)
         else: return data_date + timedelta(days=(5 - data_dow))
+    elif 'q' in expiry.lower():
+        quarter, year = expiry.split('-')
+        month = int(quarter[1])*3
+        year = int(year)
+        c = Calendar()
+        last_day = c.monthdatescalendar(year, month)[-1][-1]
+        while not (last_day.month == month and last_day.isoweekday() <= 5):
+            last_day = last_day - timedelta(days=1)
+        return last_day        
     else:
         e_dt = datetime.strptime(expiry, '%b-%y')
         c = Calendar()
@@ -112,77 +119,83 @@ def get_cid(con_dic, price_dic, session):
     return price_dic
 
 
-dburl = 'postgresql+psycopg2:///' + db_name
-engine = create_engine(dburl)
+# For running from command line
+if __name__ == "__main__":
+    file_to_parse = sys.argv[1]
+    db_name = sys.argv[2]
 
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-session = Session()
+    dburl = 'postgresql+psycopg2:///' + db_name
+    engine = create_engine(dburl)
 
-cp = ChainParser(file_to_parse)
-data_date = datetime.strptime(cp.date, '%Y-%m-%d').date()
-date_time_dic = {'date': cp.date, 'time': cp.time}
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-if not session.query(Ticker).get(cp.ticker):
-    session.add(Ticker(ticker=cp.ticker))
-    session.commit()
+    cp = ChainParser(file_to_parse)
+    data_date = datetime.strptime(cp.date, '%Y-%m-%d').date()
+    date_time_dic = {'date': cp.date, 'time': cp.time}
 
-soup = BeautifulSoup(cp.s)
-underlying_body, option_body = soup.findAll('tbody')
+    if not session.query(Ticker).get(cp.ticker):
+        session.add(Ticker(ticker=cp.ticker))
+        session.commit()
 
-if not session.query(UnderlyingPrice).get((cp.ticker, cp.date, cp.time)):
-    stock = dict([('ticker', cp.ticker)] + date_time_dic.items())
-    headers = [th.text for th in underlying_body.findAll('th')]
-    data = [td.text for td in underlying_body.findAll('td')]
-    for (h, d) in zip(headers, data):
-        if h == 'BxA Size':
-            stock['bid_size'], stock['ask_size'] = [x.strip() 
-                                                    for x in d.split('x')]
-        elif h == 'Bid [tick]':
-            stock['bid'], stock['tick'] = [x.strip(' []') 
-                                           for x in d.split('[')]
-        elif h == '% Change':
-            stock['pct_change'] = d.strip('%')
+    soup = BeautifulSoup(cp.s)
+    underlying_body, option_body = soup.findAll('tbody')
+
+    if not session.query(UnderlyingPrice).get((cp.ticker, cp.date, cp.time)):
+        stock = dict([('ticker', cp.ticker)] + date_time_dic.items())
+        headers = [th.text for th in underlying_body.findAll('th')]
+        data = [td.text for td in underlying_body.findAll('td')]
+        for (h, d) in zip(headers, data):
+            if h == 'BxA Size':
+                stock['bid_size'], stock['ask_size'] = [x.strip() 
+                                                        for x in d.split('x')]
+            elif h == 'Bid [tick]':
+                stock['bid'], stock['tick'] = [x.strip(' []') 
+                                               for x in d.split('[')]
+            elif h == '% Change':
+                stock['pct_change'] = d.strip('%')
+            else:
+                stock[h.lower()] = ''.join(d.split(','))
+
+        session.add(UnderlyingPrice(**stock))
+        session.commit()
+
+    headers = [th.text for th in option_body.findAll('th')]
+    headers = [h.lower() for h in headers]
+    c_head, p_head = headers[1:7], headers[9:-1]
+
+    data = [''.join(td.text.split(',')) for td in option_body.findAll('td')]
+    last_date = None
+    last_expiry = None
+    while len(data) >= len(headers):
+        d, data = data[0:len(headers)], data[len(headers):]
+        c_data, expiry, strike, p_data = d[1:7], d[7], d[8], d[9:-1]
+
+        if last_expiry != expiry:
+            last_expiry = expiry
+            expiry_date = get_expiry_date(last_date, data_date, expiry)
+            last_date = expiry_date
         else:
-            stock[h.lower()] = ''.join(d.split(','))
+            expiry_date = last_date
 
-    session.add(UnderlyingPrice(**stock))
+        contract = {'ticker': cp.ticker, 'expiry': expiry_date, 
+                    'strike': strike}
+
+        c_con = dict([('call_put', 'C')] + contract.items())
+        p_con = dict([('call_put', 'P')] + contract.items())
+
+        c_price = dict(zip(c_head, c_data) + date_time_dic.items())
+        p_price = dict(zip(p_head, p_data) + date_time_dic.items())
+
+        c_price = get_cid(c_con, c_price, session)
+        p_price = get_cid(p_con, p_price, session)
+
+        if not session.query(OptionPrice).get((c_price['id'], c_price['date'], 
+                                               c_price['time'])):
+            session.add(OptionPrice(**c_price))
+        if not session.query(OptionPrice).get((p_price['id'], p_price['date'], 
+                                               p_price['time'])):
+            session.add(OptionPrice(**p_price))
+
     session.commit()
-
-headers = [th.text for th in option_body.findAll('th')]
-headers = [h.lower() for h in headers]
-c_head, p_head = headers[1:7], headers[9:-1]
-
-data = [''.join(td.text.split(',')) for td in option_body.findAll('td')]
-last_date = None
-last_expiry = None
-while len(data) >= len(headers):
-    curr, data = data[0:len(headers)], data[len(headers):]
-    c_data, expiry, strike, p_data = curr[1:7], curr[7], curr[8], curr[9:-1]
-
-    if last_expiry != expiry:
-        last_expiry = expiry
-        expiry_date = get_expiry_date(last_date, data_date, expiry)
-        last_date = expiry_date
-    else:
-        expiry_date = last_date
-    
-    contract = {'ticker': cp.ticker, 'expiry': expiry_date, 'strike': strike}
-
-    c_con = dict([('call_put', 'C')] + contract.items())
-    p_con = dict([('call_put', 'P')] + contract.items())
-    
-    c_price = dict(zip(c_head, c_data) + date_time_dic.items())
-    p_price = dict(zip(p_head, p_data) + date_time_dic.items())
-
-    c_price = get_cid(c_con, c_price, session)
-    p_price = get_cid(p_con, p_price, session)
-
-    if not session.query(OptionPrice).get((c_price['id'], c_price['date'], 
-                                           c_price['time'])):
-        session.add(OptionPrice(**c_price))
-    if not session.query(OptionPrice).get((p_price['id'], p_price['date'], 
-                                           p_price['time'])):
-        session.add(OptionPrice(**p_price))
-
-session.commit()
