0ca95937d2907387e1c8dc13e82bcb98e37c759c
gazzman <sgazzisg@uw>
2013-01-07 01:20:47 -0500

    Major refactoring. Put everything in classes.

--- data_collection/parse_option_chain.py
+++ data_collection/parse_option_chain.py
@@ -70,10 +70,48 @@ class OptionPrice(Base):
 
 
 class ChainParser():
-    def __init__(self, the_file, port='5432'):
-        f = open(the_file, 'r')
-        tag_found = False
+    logger_format = ('%(levelno)s, [%(asctime)s #%(process)d]'
+                     + '%(levelname)6s -- %(threadName)s: %(message)s')
+    logger = logging.getLogger('ChainParser')
+
+    def __init__(self, filename, dbname, dbhost=''):
+        self.init_logger()
+        self.init_db_connection(dbname, dbhost)
+        self.parse_data(filename)
+        self.dt_date = datetime.strptime(self.date, '%Y-%m-%d').date()
+        up_key = (self.ticker, self.date, self.time)
+        if not self.session.query(Ticker).get(self.ticker):
+            self.add_ticker_to_db()
+        if not self.session.query(UnderlyingPrice).get(up_key):
+            self.add_up_to_db()
+        self.add_contracts_to_db()            
+
+
+    def init_logger(self):
+        hdlr = TimedRotatingFileHandler('parser_output.log', when='midnight')
+        fmt = logging.Formatter(fmt=self.logger_format)
+        hdlr.setFormatter(fmt)
+        self.logger.addHandler(hdlr)
+        self.logger.setLevel(logging.INFO)
+
+
+    def init_db_connection(self, dbname, dbhost):
+        self.logger.info('Connecting to db %s...' % dbname)
+        dburl = 'postgresql+psycopg2://%s/%s' % (dbhost, dbname)
+        self.engine = create_engine(dburl)
+        Base.metadata.create_all(self.engine)
+        Session = sessionmaker(bind=self.engine)
+        self.session = Session()
+        self.logger.info('Connected to db %s' % dbname)
+
+
+    def parse_data(self, filename):
+        self.logger.info('Parsing datafile %s...' % filename)
+        start = datetime.now()
+
+        f = open(filename, 'r')
         data = []
+        tag_found = False
         while not tag_found:
             line = f.readline().strip()
             try:
@@ -81,172 +119,149 @@ class ChainParser():
                 else: data.append(line)
             except IndexError:
                 pass
-
-        soup = BeautifulSoup(line + f.read())
-        self.underlying_body, self.option_body = soup.findAll('tbody')
-        
-        ticker, self.description, date = data
+        ticker, description, date = data
         self.ticker = '-'.join(ticker.split('/'))
         self.date, self.time = date.split('T')
+        soup = BeautifulSoup(line + f.read())
+        underlying, options = soup.findAll('tbody')
+        self.underlying_headers = [th.text for th in underlying.findAll('th')]
+        self.underlying_data = [td.text for td in underlying.findAll('td')]
+        contract_headers = [th.text.lower() for th in options.findAll('th')]
+        self.call_head = contract_headers[1:7]
+        self.put_head = contract_headers[9:-1]
+        self.con_data = [''.join(td.text.split(',')).strip('*')
+                              for td in options.findAll('td')]
+        self.num_headers = len(contract_headers)
+        self.num_contracts = 2*len(self.con_data)/self.num_headers
+
+        end = datetime.now()
+        self.logger.info('Parsed datafile %s. Took %0.3f seconds.' 
+                         % (filename, self.seconds_elapsed(start, end)))
 
 
-def get_expiry_date(last_date, data_date, expiry):
-    data_dow = data_date.isoweekday()
-    if 'week' in expiry.lower():
-        if last_date: return last_date + timedelta(days=7)
-        else: return data_date + timedelta(days=(5 - data_dow))
-    elif 'q' in expiry.lower():
-        quarter, year = expiry.split('-')
-        month = int(quarter[1])*3
-        year = int(year)
-        c = Calendar()
-        last_day = c.monthdatescalendar(year, month)[-1][-1]
-        while not (last_day.month == month and last_day.isoweekday() <= 5):
-            last_day = last_day - timedelta(days=1)
-        return last_day        
-    else:
-        e_dt = datetime.strptime(expiry, '%b-%y')
-        c = Calendar()
-        fridays = [x[4] for x in c.monthdatescalendar(e_dt.year, e_dt.month)]
-        if fridays[0].month == e_dt.month: return fridays[2]
-        else: return fridays[3]        
+    def add_ticker_to_db(self):
+        self.logger.info('Adding ticker %s' % self.ticker)
+        self.session.add(Ticker(ticker=self.ticker))
+        self.session.commit()
 
 
-def get_cid(con_dic, price_dic, session):
-    db_con = session.query(OptionContract).filter_by(**con_dic).first() 
-    if not db_con:
-        db_con = OptionContract(**con_dic)
-        session.add(db_con)
-        session.commit()
-    price_dic['id'] = db_con.id
-    return price_dic
+    def add_up_to_db(self):
+        self.logger.info('Adding the price for %s at %s'
+                            % (self.ticker, 'T'.join([self.date, self.time])))
+        start = datetime.now()
+
+        dic = {'ticker': self.ticker, 'date': self.date, 'time': self.time}
+        for (h, d) in zip(self.underlying_headers, self.underlying_data):
+            if h == 'BxA Size':
+                dic['bid_size'], dic['ask_size'] = \
+                    [''.join(x.strip().split(',')) for x in d.split('x')]
+            elif h == 'Bid [tick]':
+                dic['bid'], dic['tick'] = \
+                    [''.join(x.strip(' []').split(',')) for x in d.split('[')]
+            elif h == '% Change':
+                dic['pct_change'] = d.strip('%')
+            else:
+                dic[h.lower()] = ''.join(d.split(','))
+        self.session.add(UnderlyingPrice(**dic))
+        self.session.commit()
+
+        end = datetime.now()
+        self.logger.info('Adding price complete. Took %0.3f seconds'
+                         % self.seconds_elapsed(start, end))
 
 
-def seconds_elapsed(start, end):
-    s = (end - start).seconds
-    m = (end - start).microseconds
-    return round(s + m/1000000.0,3)
+    def add_contracts_to_db(self):
+        self.logger.info('Adding prices for %i contracts...' 
+                         % self.num_contracts) 
+        start = datetime.now()
+
+        last_date = None
+        last_expiry = None
+        self.count = 0
+        while len(self.con_data) >= self.num_headers:
+            d = self.con_data[0:self.num_headers]
+            self.con_data = self.con_data[self.num_headers:]
+            c_data, expiry, strike, p_data = d[1:7], d[7], d[8], d[9:-1]
+            if last_expiry != expiry:
+                last_expiry = expiry
+                expiry_date = self.get_expiry_date(last_date, expiry)
+                last_date = expiry_date
+            else:
+                expiry_date = last_date
+
+            base_contract = {'ticker': self.ticker, 'expiry': expiry_date, 
+                             'strike': strike}
+
+            self.add_price_to_db(base_contract, 'C', self.call_head, c_data)
+            self.add_price_to_db(base_contract, 'P', self.put_head, p_data)
+        self.session.commit()
+
+        end = datetime.now()
+        self.logger.info('Adding contract prices complete. Took %0.3f seconds' 
+                         % seconds_elapsed(start, end))
+
+
+    def add_price_to_db(self, base_contract, call_put, header, data):
+        contract = dict([('call_put', call_put)] + base_contract.items())
+        price = dict(zip(header, data)
+                     + [('date',self.date), ('time',self.time)])
+        missing = len([x for x in price.values() if x.strip() == ''])
+        if missing == 0:
+            price = self.get_cid(contract, price)
+            self.logger.debug('Got contract id %i' % price['id'])
+            sq = self.session.query(OptionPrice).get((price['id'],
+                                                 price['date'], price['time']))
+            if not sq:
+                self.session.add(OptionPrice(**price))
+                self.logger.debug('Added the price for contract %i'
+                                  % price['id'])
+        else:
+            self.logger.info('No data for %s%s%s%08i on %s' % (self.ticker, 
+                             contract['expiry'].strftime('%y%m%d'), call_put,
+                             float(contract['strike'])*1000, self.date))
+
+
+    def get_expiry_date(self, last_date, expiry):
+        data_dow = self.dt_date.isoweekday()
+        if 'week' in expiry.lower():
+            if last_date: return last_date + timedelta(days=7)
+            else: return self.dt_date + timedelta(days=(5 - data_dow))
+        elif 'q' in expiry.lower():
+            quarter, year = expiry.split('-')
+            month = int(quarter[1])*3
+            year = int(year)
+            c = Calendar()
+            last_day = c.monthdatescalendar(year, month)[-1][-1]
+            while not (last_day.month == month and last_day.isoweekday() <= 5):
+                last_day = last_day - timedelta(days=1)
+            return last_day        
+        else:
+            e_dt = datetime.strptime(expiry, '%b-%y')
+            c = Calendar()
+            fridays = [x[4] 
+                       for x in c.monthdatescalendar(e_dt.year, e_dt.month)]
+            if fridays[0].month == e_dt.month: return fridays[2]
+            else: return fridays[3]
+
+
+    def get_cid(self, con, price):
+        db_con = self.session.query(OptionContract).filter_by(**con).first() 
+        if not db_con:
+            db_con = OptionContract(**con)
+            self.session.add(db_con)
+            self.session.commit()
+        price['id'] = db_con.id
+        return price
+
+
+    def seconds_elapsed(self, start, end):
+        s = (end - start).seconds
+        m = (end - start).microseconds
+        return round(s + m/1000000.0,3)
+
 
 # For running from command line
 if __name__ == "__main__":
     file_to_parse = sys.argv[1]
     db_name = sys.argv[2]
-    logger_format = ('%(levelno)s, [%(asctime)s #%(process)d]'
-                     + '%(levelname)6s -- %(threadName)s: %(message)s')
-
-    logger = logging.getLogger('parse_option_chain')
-    hdlr = TimedRotatingFileHandler('parser_output.log', when='midnight')
-    fmt = logging.Formatter(fmt=logger_format)
-    hdlr.setFormatter(fmt)
-    logger.addHandler(hdlr)
-    logger.setLevel(logging.INFO)
-    dburl = 'postgresql+psycopg2:///' + db_name
-    engine = create_engine(dburl)
-
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    logger.info('Parsing datafile ' + file_to_parse)
-    parse_start = datetime.now()
-    cp = ChainParser(file_to_parse)
-    data_date = datetime.strptime(cp.date, '%Y-%m-%d').date()
-    date_time_dic = {'date': cp.date, 'time': cp.time}
-
-    if not session.query(Ticker).get(cp.ticker):
-        logger.info('Adding ticker ' + cp.ticker + ' to ' + db_name)
-        session.add(Ticker(ticker=cp.ticker))
-        session.commit()
-
-    o_headers = [th.text.lower() for th in cp.option_body.findAll('th')]
-    c_head, p_head = o_headers[1:7], o_headers[9:-1]
-
-    o_data = [''.join(td.text.split(',')) for td in cp.option_body.findAll('td')]
-    o_data = [d.strip('*') for d in o_data]
-    parse_end = datetime.now()
-    logger.info('Parsing complete. Took ' 
-                + str(seconds_elapsed(parse_start, parse_end)) + ' seconds.')
-
-    if not session.query(UnderlyingPrice).get((cp.ticker, cp.date, cp.time)):
-        logger.info('Adding the price for ' + cp.ticker + ' at ' 
-                    + 'T'.join([cp.date, cp.time]))
-        add_price_start = datetime.now()
-        stock = dict([('ticker', cp.ticker)] + date_time_dic.items())
-        headers = [th.text for th in cp.underlying_body.findAll('th')]
-        data = [td.text for td in cp.underlying_body.findAll('td')]
-        for (h, d) in zip(headers, data):
-            if h == 'BxA Size':
-                stock['bid_size'], stock['ask_size'] = [''.join(x.strip().split(','))
-                                                        for x in d.split('x')]
-            elif h == 'Bid [tick]':
-                stock['bid'], stock['tick'] = [''.join(x.strip(' []').split(','))
-                                               for x in d.split('[')]
-            elif h == '% Change':
-                stock['pct_change'] = d.strip('%')
-            else:
-                stock[h.lower()] = ''.join(d.split(','))
-
-        session.add(UnderlyingPrice(**stock))
-        session.commit()
-        add_price_end = datetime.now()
-        logger.info('Adding price complete. Took ' 
-                + str(seconds_elapsed(add_price_start, add_price_end))
-                + ' seconds.')
-    else: logger.info(cp.ticker + ' price data already in db for ' 
-                      + 'T'.join([cp.date, cp.time]))
-
-    logger.info('Adding prices for ' + str(len(o_data)/len(o_headers)) 
-                + ' contracts.')
-    add_prices_start = datetime.now()
-    last_date = None
-    last_expiry = None
-    while len(o_data) >= len(o_headers):
-        d, o_data = o_data[0:len(o_headers)], o_data[len(o_headers):]
-        c_data, expiry, strike, p_data = d[1:7], d[7], d[8], d[9:-1]
-
-        if last_expiry != expiry:
-            last_expiry = expiry
-            expiry_date = get_expiry_date(last_date, data_date, expiry)
-            last_date = expiry_date
-        else:
-            expiry_date = last_date
-
-        contract = {'ticker': cp.ticker, 'expiry': expiry_date, 
-                    'strike': strike}
-
-        c_con = dict([('call_put', 'C')] + contract.items())
-        p_con = dict([('call_put', 'P')] + contract.items())
-
-        c_price = dict(zip(c_head, c_data) + date_time_dic.items())
-        p_price = dict(zip(p_head, p_data) + date_time_dic.items())
-
-        # Determine if the data is missing
-        c_nc = len([x for x in c_price.values() if x.strip() == ''])
-        p_nc = len([x for x in p_price.values() if x.strip() == ''])
-
-        # Get the contract ID
-        c_price = get_cid(c_con, c_price, session)
-        logger.debug('Got contract id ' + str(c_price['id']))
-        p_price = get_cid(p_con, p_price, session)
-        logger.debug('Got contract id ' + str(p_price['id']))
-
-        # See if this price is already in the db
-        c_sq =session.query(OptionPrice).get((c_price['id'], c_price['date'], 
-                                              c_price['time']))
-        p_sq =session.query(OptionPrice).get((p_price['id'], p_price['date'], 
-                                              p_price['time']))
-
-        # Add if data isn't missing and the price isn't already in db
-        if not (c_sq or c_nc == 0):
-            session.add(OptionPrice(**c_price))
-            logger.debug('Adding the price for contract ' + str(c_price['id']))
-
-        if not (p_sq or p_nc == 0):
-            session.add(OptionPrice(**p_price))
-            logger.debug('Adding the price for contract ' + str(p_price['id']))
-
-    session.commit()
-    add_prices_end = datetime.now()
-    logger.info('Adding contract prices complete. Took ' 
-            + str(seconds_elapsed(add_prices_start, add_prices_end))
-            + ' seconds.')    
+    cp = ChainParser(file_to_parse, db_name)
