a7d2e836d56aae79137eea1f0278530787fe73be
gazzman <sgazzisg@uw>
2013-01-07 18:31:46 -0500

    First test version.

--- data_collection/pos2db.py
+++ data_collection/pos2db.py
@@ -50,7 +50,8 @@ class Position(Base):
     security_type = Column(String)
     
 class AddDBMixin():
-    def add_timezone(self, date, time, fmt, locale='US/Eastern'):
+    def add_timezone(self, date, time, fmt='%Y-%m-%dT%H:%M:%S',
+                     locale='US/Eastern'):
         tz = timezone(locale)
         dt = ' '.join([date, time])
         dt = datetime.strptime(dt, fmt)
@@ -61,7 +62,6 @@ class AddDBMixin():
         s = (end - start).seconds
         m = (end - start).microseconds
         return round(s + m/1000000.0,3)
-
 
     def fix_header(self, header):
         header = header.lower().strip()
@@ -79,7 +79,6 @@ class AddDBMixin():
         elif header == 'description': return 'name'
         else: return header
 
-
     def fix_data(self, data):
         data = ''.join(data.split(',')).strip()
         data = ''.join(data.split('$'))
@@ -90,137 +89,154 @@ class AddDBMixin():
         elif data == 'Cash & Money Market': return 'Cash'
         else: return data
 
-
-    def get_id(self, account, session):
-        db_acc = session.query(Account).filter_by(**account).first()
+    def get_id(self, account):
+        db_acc = self.session.query(Account).filter_by(**account).first()
         if not db_acc:
             db_acc = Account(**account)
-            session.add(db_acc)
-            session.commit()
+            self.session.add(db_acc)
+            self.session.commit()
         return db_acc.id
+
+    def init_logger(self, logfilename):
+        hdlr = TimedRotatingFileHandler(logfilename, when='midnight')
+        fmt = logging.Formatter(fmt=self.logger_format)
+        hdlr.setFormatter(fmt)
+        self.logger.addHandler(hdlr)
+        self.logger.setLevel(logging.INFO)
+
+    def init_db_connection(self, dbname, dbhost):
+        self.logger.info('Connecting to db %s...' % dbname)
+        dburl = 'postgresql+psycopg2://%s/%s' % (dbhost, dbname)
+        self.engine = create_engine(dburl)
+        Base.metadata.create_all(self.engine)
+        Session = sessionmaker(bind=self.engine)
+        self.session = Session()
+        self.logger.info('Connected to db %s' % dbname)
         
 class Schwab2DB(AddDBMixin):
-    pos_fname = sys.argv[1]
-    db_name = sys.argv[2]
+#    logger_format = ('%(levelno)s, [%(asctime)s #%(process)d]'
+#                     + '%(levelname)6s -- %(threadName)s: %(message)s')
+    logger_format = ('%(levelno)s, [%(asctime)s #%(process)d]'
+                     + '%(levelname)6s: %(message)s')
+    logger = logging.getLogger('Schwab2DB')
     institution = ('institution', 'Schwab')
-    pos_time = {}
+    fmt='%m/%d/%Y %H:%M:%S'
 
-    logger_format = ('%(levelno)s, [%(asctime)s #%(process)d]'
-                     + '%(levelname)6s -- %(threadName)s: %(message)s')
-    logger = logging.getLogger('schwab2db')
-    hdlr = TimedRotatingFileHandler('schwab2db.log', when='midnight')
-    fmt = logging.Formatter(fmt=logger_format)
-    hdlr.setFormatter(fmt)
-    logger.addHandler(hdlr)
-    logger.setLevel(logging.INFO)
+    def __init__(self, filename, dbname, dbhost=''):
+        self.init_logger('schwab2db.log')
+        self.init_db_connection(dbname, dbhost)
 
-    logger.info('Reading ' + pos_fname)
-    start = datetime.now()
+    def process_position_file(self, filename):
+        self.logger.info('Processing %s' % filename)
+        start = datetime.now()
 
-    # Get the timestamp
-    pos_file = open(pos_fname, 'r')
-    lines = [x.strip() for x in pos_file.read().split('\n')]
-    pos_line = [x for x in lines if 'positions' in x.lower()][0]
-    dateinfo = pos_line.split('as of ')[-1].split()[0:2]
-    pos_time['date'], pos_time['time'] = add_timezone(*dateinfo)
+        # Get the timestamp
+        pos_file = open(filename, 'r')
+        lines = [x.strip() for x in pos_file.read().split('\n')]
+        pos_line = [x for x in lines if 'positions' in x.lower()][0]
+        dateinfo = pos_line.split('as of ')[-1].split()[0:2]
+        self.pos_time = {}
+        pos_time['date'], pos_time['time'] = add_timezone(*dateinfo,
+                                                          fmt=self.fmt)
 
-    # Split up the data by account
-    accounts = [x for x in lines if 'xxxx' in x.lower()]
-    totals = [x for x in lines if 'total market value' in x.lower()]
+        # Split up the data by account
+        self.accts = [x for x in lines if 'xxxx' in x.lower()]
+        totals = [x for x in lines if 'total market value' in x.lower()]
+        starts = [lines.index(x) for x in self.accts]
+        ends = [lines[x:].index(y) + x for x,y in zip(starts, totals)]
+        ranges = zip(starts, ends)
+        data = ['\n'.join(lines[x[0]+1:x[1]]) for x in ranges]
+        data = [csv.DictReader(StringIO(x)) for x in data]
+        self.data = [[dict([(self.fix_header(z[0]), self.fix_data(z[1])) 
+                      for z in y.items() if self.fix_data(z[1])]) for y in x]
+                     for x in data]
 
-    starts = [lines.index(x) for x in accounts]
-    ends = [lines[x:].index(y) + x for x,y in zip(starts, totals)]
-    ranges = zip(starts, ends)
+        end = datetime.now()
+        self.logger.info('Processing completed. Took %0.3f seconds'
+                         % self.seconds_elapsed(start, end))
 
-    data = ['\n'.join(lines[x[0]+1:x[1]]) for x in ranges]
-    data = [csv.DictReader(StringIO(x)) for x in data]
+    def add_to_db(self):
+        # Get account ids
+        acct_keys = [dict([self.broker, ('account', x)]) for x in self.accts]
+        acct_ids = [self.get_id(x) for x in acct_keys]
+        for acct_id, d in zip(acct_ids, data):
+            for row in d:
+                try:
+                    self.session.add(Position(**dict(row.items() 
+                                                   + self.pos_time.items() 
+                                                   + [('id', acct_id)])))
+                    self.session.commit()
+                except IntegrityError as err:
+                    if 'duplicate key' in str(err):
+                        msg = "Already have position data for account id %i" 
+                        msg += " for symbol '%s' at %s"
+                        msg = msg % (acc_id, row['symbol'], ' '.join(dateinfo))
+                        self.logger.info(msg)
+                        pass
+                    else: raise err
+                    self.session.rollback()        
 
-    data = [[dict([(fix_header(z[0]), fix_data(z[1])) for z in y.items() \
-            if fix_data(z[1])]) for y in x] for x in data]
+#class Fidelity2DB(AddDBMixin):
+#    pos_fname = sys.argv[1]
+#    db_name = sys.argv[2]
+#    institution = ('institution', 'Fidelity')
+#    pos_time = {}
 
-    # Connect to db
-    dburl = 'postgresql+psycopg2:///' + db_name
-    engine = create_engine(dburl)
+#    logger_format = ('%(levelno)s, [%(asctime)s #%(process)d]'
+#                     + '%(levelname)6s -- %(threadName)s: %(message)s')
+#    logger = logging.getLogger('fidelity2db')
+#    hdlr = TimedRotatingFileHandler('fidelity2db.log', when='midnight')
+#    fmt = logging.Formatter(fmt=logger_format)
+#    hdlr.setFormatter(fmt)
+#    logger.addHandler(hdlr)
+#    logger.setLevel(logging.INFO)
 
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
+#    logger.info('Reading ' + pos_fname)
+#    start = datetime.now()
 
-    # Get account ids
-    accounts = [dict([institution, ('account', x)]) for x in accounts]
-    account_ids = [get_id(x, session) for x in accounts]
+#    # Get the timestamp from last file mod time
+#    modtime = time.localtime(os.path.getmtime(pos_fname))
+#    modtime = time.strftime('%m/%d/%Y %H:%M:%S', modtime).split()
+#    pos_time['date'], pos_time['time'] = add_timezone(*modtime)
 
-    for a, d in zip(account_ids, data):
-        for row in d:
-            try:
-                session.add(Position(**dict(row.items() + pos_time.items() 
-                                            + [('id', a)])))
-                session.commit()
-            except IntegrityError as err:
-                if 'duplicate key' in str(err):
-                    msg = 'Already have position data for account id ' + str(a)
-                    msg += ' for ticker ' + row['symbol'] 
-                    msg += ' at ' + ' '.join(dateinfo)
-                    logger.info(msg)
-                    pass
-                else: raise err
-                session.rollback()        
+#    data = csv.DictReader(open(pos_fname, 'r'))
+#    data = [dict([(y[0], y[1]) for y in x.items() if y[0]])  for x in data]
+#    data = [dict([(fix_header(y[0]), fix_data(y[1])) 
+#                   for y in x.items() if fix_data(y[1])])  for x in data]
+#    account_number = data[0]['account_name/number']
 
-class Fidelity2DB(AddDBMixin):
+#    data = [dict([(y[0], y[1]) for y in x.items() \
+#                  if y[0] != 'account_name/number'])  for x in data]
+
+#    # Connect to db
+#    dburl = 'postgresql+psycopg2:///' + db_name
+#    engine = create_engine(dburl)
+
+#    Base.metadata.create_all(engine)
+#    Session = sessionmaker(bind=engine)
+#    session = Session()
+
+#    # Get account ids
+#    account = dict([institution, ('account', account_number)])
+#    account_id = get_id(account, session)
+
+#    for row in data:
+#        try:
+#            session.add(Position(**dict(row.items() + pos_time.items() 
+#                                        + [('id', account_id)])))
+#            session.commit()
+#        except IntegrityError as err:
+#            if 'duplicate key' in str(err):
+#                msg = 'Already have position data for account id '
+#                msg += str(account_id) + ' for ticker ' + row['symbol'] 
+#                msg += ' at ' + ' '.join(modtime)
+#                logger.info(msg)
+#                pass
+#            else: raise err
+#            session.rollback()
+            
+# For running from command line
+if __name__ == "__main__":
     pos_fname = sys.argv[1]
     db_name = sys.argv[2]
-    institution = ('institution', 'Fidelity')
-    pos_time = {}
-
-    logger_format = ('%(levelno)s, [%(asctime)s #%(process)d]'
-                     + '%(levelname)6s -- %(threadName)s: %(message)s')
-    logger = logging.getLogger('fidelity2db')
-    hdlr = TimedRotatingFileHandler('fidelity2db.log', when='midnight')
-    fmt = logging.Formatter(fmt=logger_format)
-    hdlr.setFormatter(fmt)
-    logger.addHandler(hdlr)
-    logger.setLevel(logging.INFO)
-
-    logger.info('Reading ' + pos_fname)
-    start = datetime.now()
-
-    # Get the timestamp from last file mod time
-    modtime = time.localtime(os.path.getmtime(pos_fname))
-    modtime = time.strftime('%m/%d/%Y %H:%M:%S', modtime).split()
-    pos_time['date'], pos_time['time'] = add_timezone(*modtime)
-
-    data = csv.DictReader(open(pos_fname, 'r'))
-    data = [dict([(y[0], y[1]) for y in x.items() if y[0]])  for x in data]
-    data = [dict([(fix_header(y[0]), fix_data(y[1])) 
-                   for y in x.items() if fix_data(y[1])])  for x in data]
-    account_number = data[0]['account_name/number']
-
-    data = [dict([(y[0], y[1]) for y in x.items() \
-                  if y[0] != 'account_name/number'])  for x in data]
-
-    # Connect to db
-    dburl = 'postgresql+psycopg2:///' + db_name
-    engine = create_engine(dburl)
-
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    # Get account ids
-    account = dict([institution, ('account', account_number)])
-    account_id = get_id(account, session)
-
-    for row in data:
-        try:
-            session.add(Position(**dict(row.items() + pos_time.items() 
-                                        + [('id', account_id)])))
-            session.commit()
-        except IntegrityError as err:
-            if 'duplicate key' in str(err):
-                msg = 'Already have position data for account id '
-                msg += str(account_id) + ' for ticker ' + row['symbol'] 
-                msg += ' at ' + ' '.join(modtime)
-                logger.info(msg)
-                pass
-            else: raise err
-            session.rollback()
+    s2db = Schwab2DB(pos_fname, dbname)
