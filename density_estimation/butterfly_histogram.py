671fb00a78df3ef1f901f5dc504b94cafd1fe9ea
gazzman <sgazzisg@uw>
2013-01-28 00:33:50 -0500

    Improved plotting capabilities.

--- density_estimation/butterfly_histogram.py
+++ density_estimation/butterfly_histogram.py
@@ -1,39 +1,52 @@
 #!/usr/bin/python
+from finalysis.combo_gnuplots import GNUPlotBase
+
 class ButterflyHistogram():
-    outfile = 'butterhist.dat'
+    gpbase = GNUPlotBase()
+    datafile = 'butterhist.dat'
     intervals = list()
     prices = list()
     probs = list()
     price_sum = 0
-    n = int()
+    line = '%0.2f %0.3f\n'
 
     def __init__(self, strike_intervals):
         self.intervals = strike_intervals
-        self.prices = [0 for x in self.intervals]
-        self.probs = [0 for x in self.intervals]
+        self.prices = [0 for x in self.intervals][:-1]
+        self.probs = [1.0/len(self.intervals) for x in self.intervals]
+        self.gpbase.xmin = strike_intervals[0][0]*.9
+        self.gpbase.xmax = strike_intervals[-1][-1]*1.1
+        self.gpbase.ymin = 0
+        self.gpbase.ymax = 1
+        self.xticks = [x[0] for x in strike_intervals]
+        self.xticks.append(strike_intervals[-1][-1])
+        self.yticks = [x/10.0 for x in range(1, 10)]
 
-    def update_price(self, interval, price):
-        i = self.intervals.index(interval)
-        prices[i] = price
+    def update_price(self, price, index):
+        self.prices[index] = price
         self.calculate_probabilities()
-        self.plot_histogram()
 
-    def midpoint(self, interval):
-        return (interval[1] - interval[0])/2.0
-        
     def sum_prices(self):
         self.price_sum = 0
-        for x in prices: self.price_sum += x
+        for x in self.prices: self.price_sum += x
 
     def calculate_probabilities(self):
         self.sum_prices()
-        f = open(outfile, 'w')
-        for i in range(0, n+3):
-            if i == 0 or i == n+2: probs[i] = 0
-            elif i == 1 or i == n+1: probs[i] = 0.5*prices[i]/self.price_sum
-            else: probs[i] = 0.5*(prices[i-1] + prices[i])/self.price_sum
-            f.write('%0.2f %0.3f' % (self.midpoint(interval[i]), probs[i]))
+        f = open(self.datafile, 'w')
+        self.probs[0] = 0.5*self.prices[0]/self.price_sum
+        f.write(self.line % (self.mid(self.intervals[0]), self.probs[0]))
+        for i in range(1, len(self.probs)-1):
+            self.probs[i] = self.mid(self.prices[i-1:i+1])/self.price_sum
+            f.write(self.line % (self.mid(self.intervals[i]), self.probs[i]))
+        self.probs[-1] = 0.5*self.prices[-1]/self.price_sum
+        f.write(self.line % (self.mid(self.intervals[-1]), self.probs[-1]))
         f.close()
 
-    def plot_histogram(self):
-        print 'plot "%s" with boxes' % self.outfile
+    def mid(self, interval):
+        return (interval[1] + interval[0])/2.0
+
+    def plot_histogram(self, fname=None):
+        self.gpbase.set_output(fname=fname)
+        self.gpbase.gen_ticks(self.xticks, self.yticks)
+        self.gpbase.gen_header(ylabel='Probability')
+        print 'plot "%s" with boxes' % self.datafile
