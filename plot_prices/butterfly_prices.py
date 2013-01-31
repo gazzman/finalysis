d7897aa71df531e8dbad96f84dbd48e2b06afaa9
gazzman <sgazzisg@uw>
2013-01-31 10:20:47 -0500

    Allowed for negative prices.

--- plot_prices/butterfly_prices.py
+++ plot_prices/butterfly_prices.py
@@ -9,19 +9,18 @@ class ButterflyPrices():
         self.gpbase = GNUPlotBase()
         self.gpbase.xmin = strike_intervals[0][0]*.9
         self.gpbase.xmax = strike_intervals[-2][-1]*1.1
-        self.gpbase.ymin = 0
+        self.gpbase.ymin = -1*max_payoff
         self.gpbase.ymax = max_payoff
         self.datafile = 'butterprices.dat'
         self.line = '%0.2f %0.3f\n'
         self.xticks = [x[0] for x in strike_intervals 
                                          if strike_intervals.index(x) % 2 == 0]
         self.xticks.append(strike_intervals[-1][-1])
-        self.yticks = [max_payoff/5.0*x for x in range(0, 6)]
+        self.yticks = [max_payoff/5.0*x for x in range(-5, 6)]
         self.intervals[-1] = ('Spot',)
 
     def update_price(self, price, index):
-        if price > 0: self.prices[index] = price
-        else: self.prices[index] = 0
+        self.prices[index] = price
         f = open(self.datafile, 'w')
         f.write('#Underlying %0.3f\n' % self.prices[-1])
         for i in range(0, len(self.prices)-1):
