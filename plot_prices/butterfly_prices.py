c8856aa5415308b0cac71a69d6098149e7f6e20b
gazzman <sgazzisg@uw>
2013-01-31 01:19:11 -0500

    Added extra line to handle underlying price data.

--- plot_prices/butterfly_prices.py
+++ plot_prices/butterfly_prices.py
@@ -23,9 +23,11 @@ class ButterflyPrices():
         if price > 0: self.prices[index] = price
         else: self.prices[index] = 0
         f = open(self.datafile, 'w')
-        for i in range(0, len(self.prices)):
+        for i in range(0, len(self.prices)-1):
             f.write(self.line % (self.intervals[i][-1], self.prices[i]))
+        f.write('Underlying %0.3f\n' self.prices[-1])
         f.close()
+        
 
     def plot_prices(self, fname=None, timestamp=None):
         self.gpbase.set_output(fname=fname)
