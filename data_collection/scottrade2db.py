a57613e2eec450b76b80463e6ca9afb931f4df3e
gazzman <sgazzisg@uw>
2013-02-16 23:48:03 -0500

    Script now takes date from first line of datafile.

--- data_collection/scottrade2db.py
+++ data_collection/scottrade2db.py
@@ -34,15 +34,13 @@ if __name__ == "__main__":
     account_info = {'institution': 'Scottrade', 'account': acct_num}
     pos_data = {}
 
-    # Get the timestamp
-    mtime = datetime.fromtimestamp(os.path.getmtime(pos_fname))
-    date = mtime.date().strftime('%m/%d/%Y')
-    time = mtime.time().strftime('%H:%M:%S')
-    date, time = add_timezone(date, time)
-    pos_data['timestamp'] = '%s %s' % (date, time)
     pos_data['id'] = get_id(account_info, session)
 
-    c = csv.DictReader(open(pos_fname, 'r'))
+    # Get the timestamp
+    f = open(pos_fname, 'r')
+    pos_data['timestamp'] = f.readline().strip().split()[-1]
+
+    c = csv.DictReader(f)
     for row in c:
         pos_data.update(gen_position_data(row, scottrade_map))
         session.add(Position(**pos_data))
