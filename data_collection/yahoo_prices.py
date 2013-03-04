1695ca5bc9856b18aa4af260f6c0c38a202dcb89
gazzman <sgazzisg@uw>
2013-03-03 21:50:50 -0500

    Major improvements in robustness and features.
    Added support for writing data in a postgresql.
    Improved handling of missing data.

--- data_collection/yahoo_prices.py
+++ data_collection/yahoo_prices.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-__version__ = ".01"
+__version__ = ".02"
 __author__ = "gazzman"
 __copyright__ = "(C) 2013 gazzman GNU GPL 3."
 __contributors__ = []
@@ -12,8 +12,19 @@ import re
 import sys
 import urllib2
 
+try:
+    from sqlalchemy import create_engine, MetaData, Table
+    from sqlalchemy import Column, Date
+    from sqlalchemy.schema import CreateSchema
+    from sqlalchemy.exc import ProgrammingError
+    from sqlalchemy.dialects.postgresql import NUMERIC, VARCHAR
+    have_alchemy = True
+except ImportError:
+    have_alchemy = False
+
 UA = 'Mozilla/5.0(X11; Linux x86_64; rv:10.0.12)Gecko/20130109 Firefox/10.0.12'
 HEADER = {'User-Agent' : UA}
+MAX_RETRY = 7
 
 class YahooData:
     dfmt = '%Y%m%d'
@@ -22,43 +33,47 @@ class YahooData:
     url += '&d=%(to_month)i&e=%(to_day)i&f=%(to_year)i&g=%(info)s&ignore=.csv'
 
     def grab_data(self, tickerlist, from_date=None, frequency='d'):
-        tickerlist
         to = datetime.now()
+        f = StringIO()
         if not from_date: fr = datetime.strptime('19000101', self.dfmt)
         else: fr = datetime.strptime(from_date, self.dfmt)
         dates = {'to_year': to.year, 'to_month': to.month-1, 'to_day': to.day,
                  'fr_year': fr.year, 'fr_month': fr.month-1, 'fr_day': fr.day}
-        f = open('yprices_' + to.strftime('%Y%m%dT%H%M%S') + '.csv', 'w')
         csvout = csv.DictWriter(f, [])
         firstrun = True
         for ticker in tickerlist:
             print >> sys.stderr, 'Starting %s...' % ticker, 
-            divdic = {}
-            divinfo = {'ticker': ticker, 'info': 'v'}
-            divinfo.update(dates)
-            divurl = self.url % divinfo
-            divpage = self.pull_page(divurl)
-            dividends = self.send_to_csv(divpage)
-            for dividend in dividends:
-                divdic[dividend['date']] = dividend['dividends']
-
             priceinfo = {'ticker': ticker, 'info': frequency}
             priceinfo.update(dates)
             priceurl = self.url % priceinfo
             pricepage = self.pull_page(priceurl)
-            prices = self.send_to_csv(pricepage)
-            if firstrun:
-                headers = (['ticker', 'dividends'] + prices.fieldnames)
-                f.write(','.join(headers) + '\n')
-                csvout.fieldnames = headers
-                firstrun = False
-            for price in prices:
-                price['ticker'] = ticker
-                if price['date'] in divdic:
-                    price['dividends'] = divdic[price['date']]
-                csvout.writerow(price)
-            print >> sys.stderr, 'Done!'
-        f.close()
+            if pricepage:
+                prices = self.send_to_csv(pricepage)
+
+                divdic = {}
+                divinfo = {'ticker': ticker, 'info': 'v'}
+                divinfo.update(dates)
+                divurl = self.url % divinfo
+                divpage = self.pull_page(divurl)
+                dividends = self.send_to_csv(divpage)
+                for dividend in dividends:
+                    divdic[dividend['date']] = dividend['dividends']
+
+                if firstrun:
+                    headers = (['ticker', 'dividends'] + prices.fieldnames)
+                    f.write(','.join(headers) + '\n')
+                    csvout.fieldnames = headers
+                    firstrun = False
+                for price in prices:
+                    price['ticker'] = ticker
+                    if price['date'] in divdic:
+                        price['dividends'] = divdic[price['date']]
+                    csvout.writerow(price)
+                print >> sys.stderr, 'Done!'
+            else:
+                print >> sys.stderr, '\nProblem with %s. Skipping.' % ticker
+        f.seek(0)
+        return f
 
     def send_to_csv(self, page):
         s = page.read()
@@ -68,9 +83,9 @@ class YahooData:
 
     def pull_page(self, url):
         req = urllib2.Request(url, headers=HEADER)
-        count = 0
-        while True:
-            if count > 4:
+        count = 1
+        while count < MAX_RETRY:
+            if 4 < count:
                 print ' '.join(["\nOk, we're on try", str(count),
                                 "now.\nWhy don't you see if this url,",
                                 url, "is even working?"])
@@ -89,8 +104,10 @@ class YahooData:
 if __name__ == "__main__":
     yd = YahooData()
     description = 'Pull ticker price and dividend data from Yahoo! Finance.'
-    from_help = 'Specify date to pull data from. Format is %s'\
+    from_help = 'specify date to pull data from. Format is %s'\
                                                    % yd.dfmt.replace('%', '%%')
+    db_help = 'the name of a postgresql database.'
+    schema_help = 'an optional database schema.'
 
     p = argparse.ArgumentParser(description=description)
     p.add_argument('tickerfile', type=str, 
@@ -103,13 +120,64 @@ if __name__ == "__main__":
                             help='Pull weekly data')
     freq_group.add_argument('--monthly', action='store_true',
                             help='Pull monthly data')
+    db_group = p.add_argument_group()
+    db_group.add_argument('--database', help=db_help)
+    db_group.add_argument('--schema', help=schema_help)
+
     args = p.parse_args()
 
     with open(args.tickerfile, 'r') as f:
         tickers = [x.strip() for x in f.read().split('\n') if x.strip() != '']
     tickers = [x.upper() for x in tickers if x[0] != '#']    
 
-    if args.weekly: freq = 'w'
-    elif args.monthly: freq = 'm'
-    else: freq = 'd'
-    yd.grab_data(tickers, from_date=args.fromdate, frequency=freq)
+    if args.weekly:
+        freq = 'w'
+        tablename = 'yahoo_weekly_prices'
+    elif args.monthly:
+        freq = 'm'
+        tablename = 'yahoo_monthly_prices'
+    else:
+        freq = 'd'
+        tablename = 'yahoo_daily_prices'
+
+    d = yd.grab_data(tickers, frequency=freq, from_date=args.fromdate)
+    if args.database and have_alchemy:
+        dburl = 'postgresql+psycopg2:///' + args.database
+        engine = create_engine(dburl)
+        try: engine.execute(CreateSchema(args.schema))
+        except ProgrammingError: pass
+        metadata = MetaData(engine)
+        table = Table(tablename, metadata,
+                      Column('ticker', VARCHAR(21), index=True, 
+                             primary_key=True),
+                      Column('date', Date, index=True, primary_key=True),
+                      Column('dividends', NUMERIC(19,4)),
+                      Column('open', NUMERIC(19,4)),
+                      Column('high', NUMERIC(19,4)),
+                      Column('low', NUMERIC(19,4)),
+                      Column('close', NUMERIC(19,4)),
+                      Column('volume', NUMERIC(19,4)),
+                      Column('adj_close', NUMERIC(19,4)),
+                      schema=args.schema)
+        metadata.create_all()
+        headers = d.readline().strip().split(',')
+        headers = [x.replace(' ', '_') for x in headers]
+
+        # Delete old price data
+        delete = table.delete()
+        conn = engine.connect()
+        for ticker in tickers:
+            conn.execute(delete, ticker=ticker)
+
+        # Write new price data
+        if args.schema: tablename = '%s.%s' % (args.schema, tablename)
+        conn = metadata.bind.raw_connection()
+        cur = conn.cursor()
+        cur.copy_from(d, tablename, sep=',', null='', columns=headers)
+        conn.commit()
+    elif args.database:
+        raise EnvironmentError("Python is unable to import sqlalchemy")
+    else:
+        to = datetime.now()
+        fname = 'yprices_%s.csv' % to.strftime('%Y%m%dT%H%M%S')
+        with open(fname, 'w') as outfile: outfile.write(d.read())
