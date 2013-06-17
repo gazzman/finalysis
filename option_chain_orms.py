478c15cc4c181d24c869407e7ed3795c8af2333d
gazzman <sgazzisg@uw>
2013-06-17 01:07:12 -0400

    Added an expiry column as primary key and changed the shows.

--- option_chain_orms.py
+++ option_chain_orms.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 from sqlalchemy import Table
-from sqlalchemy import Column, DateTime
+from sqlalchemy import Column, Date, DateTime
 from sqlalchemy.dialects.postgresql import NUMERIC, VARCHAR
 
 def gen_table(tablename, metadata, links=16, right='b', schema=None):
     '''Generate an option chain table
-    
+
     tablename   -- the name of the table
     metadata    -- the sqlalchemy MetaData() object
     links       -- the total number of strike prices in chain
@@ -18,10 +18,12 @@ def gen_table(tablename, metadata, links=16, right='b', schema=None):
 
     if right == 'b': rights = ['c', 'p']
     else: rights = [right]    
-
+    
     shows = ['%s_%s' % (sh, ohlc) for sh in ['trades', 'bid', 'ask'] 
-                                  for ohlc in ['open', 'high', 'low', 'close']]
-    shows += ['volume', 'wap', 'count', 'hasGaps']
+                                  for ohlc in ['open', 'high', 'low', 
+                                               'close', 'hasGaps']]
+    shows += ['volume', 'wap', 'count']
+
     undercols = [Column(sh, NUMERIC(19, 4)) for sh in shows]
     linkcols = [Column('%s_%s_%s' % (cp, n, show), NUMERIC(19, 4)) 
                 for cp in rights  for n in range(0, links+1) for show in shows]
@@ -29,12 +31,13 @@ def gen_table(tablename, metadata, links=16, right='b', schema=None):
     return Table(tablename, metadata,
                  Column('underlying', VARCHAR(21), index=True, 
                         primary_key=True),
+                 Column('osi_underlying', VARCHAR(21)),
                  Column('timestamp', DateTime(timezone=True), index=True, 
                         primary_key=True),
-                 Column('osi_underlying', VARCHAR(21)),
                  Column('strike_start', NUMERIC(19,4), index=True, 
                         primary_key=True),
                  Column('strike_interval', NUMERIC(19,4), index=True, 
                         primary_key=True),
+                 Column('expiry', Date, index=True, primary_key=True),
                  *datacols,
                  schema=schema)
