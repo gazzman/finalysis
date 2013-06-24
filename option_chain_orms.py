ea8bb72f26c3458e6942c6b13f6ded69da7dca22
gazzman <sgazzisg@uw>
2013-06-24 09:54:02 -0400

    Removed the underlying price data rows and changed the primary key.

--- option_chain_orms.py
+++ option_chain_orms.py
@@ -9,8 +9,7 @@ SHOWTUPLE = [('trades', BAR + ('volume', 'wap', 'count')),
              ('ask', BAR)]
 SHOWCOLS = ['%s_%s' % (show[0], bar) for show in SHOWTUPLE for bar in show[1]]
 
-
-def gen_table(tablename, metadata, links=16, right='b', schema=None):
+def gen_table(tablename, metadata, links=32, right='b', schema=None):
     '''Generate an option chain table
 
     tablename   -- the name of the table
@@ -26,14 +25,6 @@ def gen_table(tablename, metadata, links=16, right='b', schema=None):
     if right == 'b': rights = ['c', 'p']
     else: rights = [right]    
 
-#    undercols = [Column(sh, NUMERIC(19, 4)) for sh in SHOWCOLS]
-#    linkcols = [Column('%s_%02i_%s' % (cp, n, sh), NUMERIC(19, 4)) 
-#                for cp in rights  for n in range(0, links+1) 
-#                for sh in SHOWCOLS]
-    undercols = []
-    for sh in SHOWCOLS:
-        if 'hasgaps' not in sh: undercols += [Column(sh, NUMERIC(19, 4))]
-        else: undercols += [Column(sh, Boolean)]
     linkcols = []
     for cp in rights:
         for n in range(0, links):
@@ -42,7 +33,6 @@ def gen_table(tablename, metadata, links=16, right='b', schema=None):
                 if 'hasgaps' not in sh: linkcols += [Column(c, NUMERIC(19, 4))]
                 else: linkcols += [Column(c, Boolean)]
 
-    datacols = undercols + linkcols
     return Table(tablename, metadata,
                  Column('underlying', VARCHAR(21), index=True,
                         primary_key=True),
@@ -50,10 +40,9 @@ def gen_table(tablename, metadata, links=16, right='b', schema=None):
                         primary_key=True),
                  Column('timestamp', DateTime(timezone=True), index=True, 
                         primary_key=True),
-                 Column('strike_start', NUMERIC(19,4), index=True, 
-                        primary_key=True),
                  Column('strike_interval', NUMERIC(19,4), index=True, 
                         primary_key=True),
                  Column('expiry', Date, index=True, primary_key=True),
-                 *datacols,
+                 Column('strike_start', NUMERIC(19,4), index=True),
+                 *linkcols,
                  schema=schema)
