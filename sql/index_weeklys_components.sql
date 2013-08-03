fc0c9f88baca05e8150c4f5fe3727029b0b68d4c
gazzman <sgazzisg@uw>
2013-08-03 09:14:06 -0400

    SQL function for determining index components with weeklys.

---
+++ sql/index_weeklys_components.sql
@@ -0,0 +1,44 @@
+CREATE FUNCTION fund_research.index_weeklys_components()
+RETURNS TABLE(ticker VARCHAR(21), 
+              alt_ticker VARCHAR(21), 
+              type VARCHAR, 
+              djx smallint, 
+              oex smallint, 
+              spx smallint, 
+              indexed smallint)
+AS $$
+      SELECT ticker, 
+             alt_ticker, 
+             type, 
+             cast(sum(djx) AS smallint), 
+             cast(sum(oex) AS smallint), 
+             cast(sum(spx) AS smallint), 
+             cast(1 AS smallint)
+      FROM (SELECT ticker, 
+                   alt_ticker, 
+                   type, 
+                   1 AS djx, 
+                   0 AS oex, 
+                   0 AS spx 
+            FROM djx_weeklys_components() 
+            UNION 
+            SELECT ticker, 
+                   alt_ticker, 
+                   type, 
+                   0, 
+                   1, 
+                   0 
+            FROM oex_weeklys_components() 
+            UNION 
+            SELECT ticker, 
+                   alt_ticker, 
+                   type, 
+                   0, 
+                   0, 
+                   1 
+            FROM spx_weeklys_components()) 
+      AS uuu 
+      GROUP BY ticker, alt_ticker, type
+      ORDER BY ticker;
+   $$
+LANGUAGE SQL;
